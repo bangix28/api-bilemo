9bcacab8df3c072b5836449ef23e7083336d1989
granger kenolane <kenolane28@gmail.com>
2021-03-22 19:12:59 +0100

    add controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,88 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\Product;
+    use App\Repository\ProductRepository;
+    use Doctrine\ORM\EntityManagerInterface;
+    use FOS\RestBundle\Controller\AbstractFOSRestController;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Component\Validator\Validator\ValidatorInterface;
+    use FOS\RestBundle\Controller\Annotations as Rest;
 
-class ProductController extends AbstractController
+
+class ProductController extends AbstractFOSRestController
 {
     /**
-     * @Route("/product", name="product")
+     * @var EntityManagerInterface
      */
-    public function index(): Response
+    private $manager;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    public function __construct(EntityManagerInterface $manager, ValidatorInterface $validator)
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $this->manager = $manager;
+        $this->validator = $validator;
+    }
+
+    /**
+     * @Rest\Get(
+     *     path="/products",
+     *     name="Product_list"
+     *     )
+     * @Rest\View()
+     */
+    public function listProduct(ProductRepository $Product)
+    {
+        return $Product->findAll();
+    }
+
+    /**
+     * @Rest\Get(
+     *     path="/Products/{Product}",
+     *     name="Product_detail"
+     * )
+     * @Rest\View()
+     */
+
+    public function detailProduct(Product $Product){
+        return $Product;
+    }
+    /**
+     * @Rest\Post(
+     *     path="/Products",
+     *     name="Product_create"
+     * )
+     * @ParamConverter("Product",converter="fos_rest.request_body")
+     * @Rest\View(statusCode=201)
+     */
+    public function createProduct(Product $Product)
+    {
+        $error = $this->validator->validate($Product);
+        if (count($error))
+        {
+            return $this->view($error, Response::HTTP_BAD_REQUEST);
+        }
+        $this->manager->persist($Product);
+        $this->manager->flush();
+        return $this->view($Product, Response::HTTP_CREATED, ['Location' => $this->generateUrl('Product_detail', ['article' => $Product->getId()])]);
+    }
+
+    /**
+     * @Rest\Delete(
+     *     path="/Products/{Product}/delete",
+     *     name="Product_delete"
+     * )
+     * @Rest\View(statusCode=201)
+     */
+    public function deleteProduct(Product $Product)
+    {
+        $this->manager->remove($Product);
+        $this->manager->flush();
     }
 }
