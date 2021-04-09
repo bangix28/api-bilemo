daf64a9dd7bde143b5f45d5783c326b838dd0896
granger kenolane <kenolane28@gmail.com>
2021-04-09 21:15:12 +0200

    create key for lexik jwt

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
