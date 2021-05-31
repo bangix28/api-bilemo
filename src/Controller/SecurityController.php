1e1ac673faaa7c8f1d2d671583d855398c7082b1
granger kenolane <kenolane28@gmail.com>
2021-05-31 14:25:39 +0200

    change extend class

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class SecurityController extends AbstractController
+class SecurityController extends AbstractFOSRestController
 {
+
     /**
-     * @Route("/security", name="security")
+     * @Route(name="api_login",path="/api/login_check")
+     * @return JsonResponse
      */
-    public function index(): Response
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+    public function api_login(): JsonResponse {
+        $user = $this->GetUser();
+        return new JsonResponse([
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
         ]);
     }
+
+
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,77 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Customer;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use JMS\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
-class UserController extends AbstractController
+
+class UserController extends AbstractFOSRestController
 {
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    private $encoder;
+
+    private $serializer;
+
+    public function __construct(EntityManagerInterface $manager, ValidatorInterface $validator, UserPasswordEncoderInterface $encoder, SerializerInterface $serializer)
+    {
+        $this->manager = $manager;
+        $this->validator = $validator;
+        $this->encoder = $encoder;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @Rest\Post(
+     *     path="/user_register",
+     *     name="User_create"
+     * )
+     * @ParamConverter("user",converter="fos_rest.request_body")
+     * @Rest\View(statusCode=201)
+     */
+    public function api_register(Request $request,User $user)
+    {
+        $data = $this->serializer->deserialize($request->getContent(),'array','json');
+        $customer = $this->manager->getRepository(Customer::class)->findOneBy(array('id'=> $data['id_customer']));
+        $user->setCustomer($customer);
+        $hash_pass = $this->encoder->encodePassword($user,$user->getPassword());
+        $user->setPassword($hash_pass);
+        $error = $this->validator->validate($user);
+        if (count($error))
+        {
+            return $this->view($error, Response::HTTP_BAD_REQUEST);
+        }
+        $this->manager->persist($user);
+        $this->manager->flush();
+        return $this->view($user, Response::HTTP_CREATED, ['Location' => $this->generateUrl('User_detail', ['User' => $user->getId()])]);
+    }
+
+    /**
+     * @Rest\Get(
+     *     path="/users/{User}",
+     *     name="User_detail"
+     * )
+     * @Rest\View()
+     */
+    public function detailUser(User $user){
+        return $user;
+    }
 
 }
