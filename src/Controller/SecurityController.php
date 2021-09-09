8410cb54691047eca9e31fdd5df25cbee8836890
granger kenolane <kenolane28@gmail.com>
2021-09-09 22:44:02 +0200

    delete useless import

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 use OpenApi\Annotations as OA;
 
 
